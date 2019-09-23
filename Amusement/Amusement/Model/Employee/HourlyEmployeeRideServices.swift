05820e211edadb41c44ec74e2f016c624b53dbb8
Michael R.F <mrflowers22@uwalumni.com>
2019-09-22 20:35:09 -0600

    implemented manger class.

---
+++ Amusement/Amusement/Model/Employee/HourlyEmployeeRideServices.swift
@@ -0,0 +1,51 @@
+//
+//  HourlyEmployeeRideServices.swift
+//  Amusement
+//
+//  Created by Michael Flowers on 9/22/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class HourlyEmployeeRideServices: Entrant {
+    
+    init(employeeInformation: EmployeeInformation) throws {
+        super.init(rideAccess: [.allRides], areaAccess: [.amusement, .rideControl], discount: [.foodDiscount, .merchandiseDiscount])
+        
+        guard employeeInformation.firstName != nil else {
+            throw AmusementParkError.noFirstNameProvided(description: "Please provide first name.")
+        }
+        guard employeeInformation.lastName != nil else {
+            throw AmusementParkError.noLastNameProvided(description: "Please Provide last name.")
+        }
+        guard employeeInformation.streetAddress != nil else {
+            throw AmusementParkError.incompleteHomeAddress(description: "Sreet address missing.")
+        }
+        guard employeeInformation.state != nil else {
+            throw AmusementParkError.incompleteHomeAddress(description: "State missing")
+        }
+        guard employeeInformation.zipCode != nil else {
+            throw AmusementParkError.incompleteHomeAddress(description: "Zipcode missing.")
+        }
+    }
+    
+    override func swipe(rideAccess: RideAccess) -> Bool {
+        return !self.rideAccess.isEmpty
+    }
+    
+    override func swipe(areaAccess: AreaAccess) -> Bool {
+        return !self.areaAccess.isEmpty
+    }
+    
+    override func swipe(discount: Discount) -> Float {
+        if discount == .foodDiscount {
+            return 0.15
+        }
+        
+        if discount == .merchandiseDiscount {
+            return 0.25
+        }
+        return 0.0
+    }
+}
