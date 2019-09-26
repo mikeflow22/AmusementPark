655ae49f5ac320d7faa7650c83eea9c01a5a8f41
Michael R.F <mrflowers22@uwalumni.com>
2019-09-25 18:20:01 -0600

    added PassType source code file

---
+++ Amusement/Amusement/Model/Model Types/PassType.swift
@@ -0,0 +1,17 @@
+//
+//  PassType.swift
+//  Amusement
+//
+//  Created by Michael Flowers on 9/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+enum PassType: String {
+    case classicGuest = "Classic Geust"
+    case vipguest = "VIP Guest"
+    case freeChild = "Free Child"
+    case hourlyEmployeeFoodServices = "Hourly Food Service"
+    case hourlyEmplyoeeRideServices = "Hourly Ride Service"
+    case hourlyEmployeeMaintenance = "Hourly Maintenance"
+}
